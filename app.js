ccd986122c6ab2b5126d48902ba7e26cbd36ea84
13circle <13circle97@gmail.com>
2020-11-30 21:37:18 +0900

    Move module alias registration to the top layer

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-require("module-alias/register");
-
 const indexRouter = require('@routes/index');
 const usersRouter = require('@routes/users');
 
