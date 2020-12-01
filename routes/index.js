d9aec280939118d455816ace56e393ae48f23489
13circle <13circle97@gmail.com>
2020-12-01 22:03:11 +0900

    Access S3 bucket path by URL

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,17 @@ const router = express.Router();
 require("dotenv").config();
 
 const s3BaseUrl = process.env.S3_BASE_URL;
+const title = "13circle Content Viewer";
 
 /* GET home page. */
 router.get("/", async (req, res, next) => {
   const dir = await s3Handler.listFolders("/");
-  res.render("index", { title: "Express", dir });
+  res.render("index", { title, dir });
+});
+
+router.get("/:path", async (req, res, next) => {
+  const dir = await s3Handler.listFolders("/" + req.params.path);
+  res.render("index", { title, dir });
 });
 
 module.exports = router;
