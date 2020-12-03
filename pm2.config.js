4993bd487b9ad60ad41c252144c4826fb39204fd
13circle <13circle97@gmail.com>
2020-12-03 12:21:27 +0900

    pm2 configuration for deploy

--- pm2.config.js
+++ pm2.config.js
@@ -10,4 +10,15 @@ module.exports = {
 			time: true,
 		},
 	],
+
+	deploy: {
+		staging: {
+			user: "root",
+			host: "3.128.164.186:5252",
+			repo: "git@github.com:13circle/content-viewer.git",
+			ref: "origin/main",
+			path: "/home/ubuntu/deploy",
+			"post-deploy": "yarn && pm2 reload pm2.config.js",
+		},
+	},
 };
