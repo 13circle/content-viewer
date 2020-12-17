97747ee53c44d076d77973bbaecf5e36abfc1cc9
13circle <13circle97@gmail.com>
2020-12-17 09:52:53 +0900

    Handle resize

--- public/javascripts/fileViewer.js
+++ public/javascripts/fileViewer.js
@@ -1,8 +1,15 @@
 var viewer;
 
+function resizeViewerHeight() {
+  var maxH = window.innerHeight - 110;
+  viewer.style.height = maxH + "px";
+}
+
 window.onload = function () {
   viewer = document.getElementById("viewer");
-  var maxH =
-    window.screen.availHeight - (window.outerHeight - window.innerHeight) - 110;
-  viewer.style.height = maxH + "px";
+  resizeViewerHeight();
+};
+
+window.onresize = function () {
+  resizeViewerHeight();
 };
