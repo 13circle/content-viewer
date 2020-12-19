ea8e979cd977657f9a5d97171b55294e92ee8e08
13circle <13circle97@gmail.com>
2020-12-20 00:32:08 +0900

    Add /users APIs (incomplete)

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,42 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
+const title = "13circle Content Viewer";
+
+router.get("/", (req, res, next) => {
+  res.render("login", { title });
+});
+
+router.get("/register", (req, res, next) => {
+  res.render("userinfo", { title, isNew: true });
+});
+
+router.get("/edit", (req, res, next) => {
+  res.render("userinfo", { title, isNew: false });
+});
+
+router.post("/register", (req, res, next) => {
+  //
+});
+
+router.delete("/unregister", (req, res, next) => {
+  //
+});
+
+router.post("/login", (req, res, next) => {
+  //
+});
+
+router.get("/check", (req, res, next) => {
+  //
+});
+
+router.patch("/edit", (req, res, next) => {
+  //
+});
+
+router.post("/logout", (req, res, next) => {
+  //
 });
 
 module.exports = router;
